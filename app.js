cade22a843446f7cefebf4b0eaf736f6a84b6aa5
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-05-23 17:21:06 -0700

    Error handling complete

--- app.js
+++ app.js
@@ -16,12 +16,33 @@ app.get('/about', (req, res) => {
     res.render('about', { projects });
 })
 
-app.get('/projects/:id', (req, res) => {
+app.get('/projects/:id', (req, res, next) => {
     const projectId = req.params.id;
     const project = projects[projectId];
+
+    if (!project) {
+        const err = new Error('Project not found.');
+        err.status = 404;
+        return next(err);
+    }
+
     res.render('project', { project });
-   
 })
+
+//404 error handler 
+app.use((req, res, next) => {
+    const err = new Error('The page you requested doesnt exist.');
+    err.status = 404;
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+    err.status = err.status || 500;
+    err.message = err.message || 'There was a Server Error'
+    res.status(err.status).send(`<h1>Error ${err.status}</h1><p>${err.message}</p>`);
+})
+
+
 
 app.listen(3000, () => {
     console.log('App is running on http://localhost:3000');
